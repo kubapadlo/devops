e2f123170d062e3f27e9c69f67d1d7b2ab9d52f3
Kuba Padło <jpadlo@student.agh.edu.pl>
2026-05-17 15:27:51 +0200

    TERRAFORM WORKS
    * infra
    
    * lil fix
    
    * optional registry image push
    
    * fix jenkinsfile
    
    * add terraform plugin to jenkinsfile
    
    * double agent section fix
    
    * remove kvm
    
    * lets go
    
    * bruh
    
    * stranger things
    
    * almost there
    
    * almost there
    
    * move proxmox from virtualbox to hyperv to use KVM
    
    * ip finding problem
    
    * loopback ip problem fix

---
+++ infrastructure/terraform/main.tf
@@ -0,0 +1,66 @@
+data "proxmox_virtual_environment_vms" "template" {
+  node_name = "pve"
+  filter {
+    name   = "name"
+    values = ["ubuntu-template"]
+  }
+}
+
+resource "proxmox_virtual_environment_vm" "moja_vm" {
+  name      = "nextjs-server"
+  node_name = "pve"
+  
+  started   = true 
+
+  clone {
+    vm_id = data.proxmox_virtual_environment_vms.template.vms[0].vm_id
+    full  = false
+    retries = 2
+  }
+
+  cpu {
+    cores = 2
+    type  = "host"
+  }
+
+  memory {
+    dedicated = 2048
+  }
+
+  network_device {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
+  # To jest kluczowe dla pobierania IP przez Jenkinsa
+  agent {
+    enabled = true
+    timeout = "15m"
+  }
+
+  initialization {
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+
+    user_account {
+      username = "ubuntu"
+      keys     = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL/eOJZJzV4dgnK+mLmWUbUZN9l6ar8oxr1P9W1tBwEI jpadlo@student.agh.edu.pl"]
+    }
+  }
+}
+
+output "vm_ip" {
+  value = try(
+    [for addr in flatten(proxmox_virtual_environment_vm.moja_vm.ipv4_addresses) : 
+      addr if addr != "127.0.0.1" && !can(regex(":", addr))
+    ][0], 
+    ""
+  )
+}
+
+output "vm_id" {
+  value = proxmox_virtual_environment_vm.moja_vm.vm_id
+}
---
+++ infrastructure/terraform/provider.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "~> 0.78"
+    }
+  }
+}
+
+variable "api_token" {
+  description = "Proxmox API token"
+  sensitive   = true
+}
+
+variable "root_password" {
+  description = "Hasło root Proxmox SSH"
+  sensitive   = true
+}
+
+provider "proxmox" {
+  endpoint  = "https://172.31.30.52:8006/" 
+  api_token = var.api_token
+  insecure  = true
+
+  ssh {
+    agent    = false
+    username = "root"
+    password = var.root_password
+  }
+}
